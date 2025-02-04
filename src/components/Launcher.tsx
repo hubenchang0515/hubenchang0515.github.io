62c23c4b78cb2fecd008ace052682347a61e95aa
PlanC <hubenchang0515@outlook.com>
2025-02-04 17:45:45 +0000

    调整 launcher 的尺寸

--- src/components/Launcher.tsx
+++ src/components/Launcher.tsx
@@ -24,7 +24,7 @@ export function Launcher(props:LauncherProps) {
                     position:'absolute',
                     bottom: 0,
                     left: 0,
-                    width: {xs:'100%', lg:'480px'},
+                    width: {xs:'100%', md:'480px'},
                     height: {xs:'100%', lg:'720px'},
                     zIndex: 9999,
                     display: 'flex'
