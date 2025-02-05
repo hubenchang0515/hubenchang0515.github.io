999f98579ab57498d2d15aad84cb5ce773888328
PlanC <hubenchang0515@outlook.com>
2025-02-05 07:13:16 +0000

    触屏适配

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -45,6 +45,19 @@ export function Window(props:WindowProps) {
         props.onMove?.(props.state.x + dx, props.state.y + dy);
     }, [props.onMove]);
 
+    const touchMove = useCallback((ev:any) => {
+        if (isNaN(pos.current.x) || isNaN(pos.current.y)) {
+            return;
+        }
+
+        const dx = ev.touches[0].clientX - pos.current.x;
+        const dy = ev.touches[0].clientY - pos.current.y;
+        pos.current.x = ev.touches[0].clientX; 
+        pos.current.y = ev.touches[0].clientY;
+        props.onMove?.(props.state.x + dx, props.state.y + dy);
+    }, [props.onMove]);
+
+
     return (
         <Fade in={!props.state.minimum}>
             <Paper 
@@ -63,9 +76,10 @@ export function Window(props:WindowProps) {
                 }}
                 onMouseDown={() => {
                     props.onClick?.();
-                    console.log(props.state.focus)
                 }}
-                
+                onTouchStart={() => {
+                    props.onClick?.();
+                }}
             >
                 <Box display='flex' alignItems='center' sx={{userSelect:'none'}}
                     onMouseDown={(ev:any) => {
@@ -82,6 +96,24 @@ export function Window(props:WindowProps) {
                             document.removeEventListener("mouseup", mouseUp);
                         }
                         document.addEventListener("mouseup", mouseUp);
+                    }}
+
+                    onTouchStart={(ev:any) => {
+                        props.onClick?.();
+                        pos.current.x = ev.touches[0].clientX; 
+                        pos.current.y = ev.touches[0].clientY;
+                        document.addEventListener("touchmove", touchMove);
+
+                        const touchUp = () => {
+                            props.onClick?.();
+                            pos.current.x = NaN; 
+                            pos.current.y = NaN;
+                            document.removeEventListener("touchmove", touchMove);
+                            document.removeEventListener("touchend", touchUp);
+                            document.removeEventListener("touchcancel", touchUp);
+                        }
+                        document.addEventListener("touchend", touchUp);
+                        document.addEventListener("touchcancel", touchUp);
                     }}
                 >
                     <img style={{width:32, height:32, paddingLeft:8, paddingRight:8}} src={props.state.icon}></img>
