364f7fd9d7438794a4da65b0bad44723cce0567f
PlanC <hubenchang0515@outlook.com>
2025-02-07 13:04:42 +0000

    窗口初始状态可配置

--- src/features/Application.ts
+++ src/features/Application.ts
@@ -4,6 +4,14 @@ export interface ApplicationProps {
     subtitle?:string;
     poor?:boolean; // 性能较差，缩放时需要隐藏内容
 
+    // 初始状态
+    x?:number;
+    y?:number;
+    width?:number;
+    height?:number;
+    maximum?:boolean;
+
+    // 内容
     url?:string;
     component?: any;
     componentProps?: any;
