788bb2883ae32382179e8fab96358d42b92bcad2
PlanC <hubenchang0515@outlook.com>
2025-05-17 15:12:31 +0000

    临时自动跳转

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.tsx'
 
-window.location.replace("https://xplanc.org/primers/document/en/02.Python");
+if (navigator.language === 'zh-CN') {
+    window.location.replace("https://xplanc.org/primers/document/zh/02.Python");
+} else {
+    window.location.replace("https://xplanc.org/primers/document/en/02.Python");
+}
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
